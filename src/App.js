16f9746c58def4cd85d873419a79737eae848101
bchavez567 <85799655+bchavez567@users.noreply.github.com>
2021-11-05 22:49:21 -0300

    Subida de app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          "Proyecto Bendita Sucu"
+        </p>
+        
+        <p>
+          Walter Chavez Comison 16990
+        </p>
+        <a
+          className="App-link"
+          href="https://instagram.com/bendita.sucu?utm_medium=copy_link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram Bendita Sucu ..
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
